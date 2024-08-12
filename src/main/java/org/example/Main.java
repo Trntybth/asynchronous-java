3a71370dcc51d48f309d4dc51b63a6f7c593a9c1
Trntybth <trinitybooth89@gmail.com>
2024-08-12 11:36:53 +0100

    feat: completed task 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,13 @@ package org.example;
 
 import java.util.Comparator;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.BiFunction;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 /*        CompletableFuture.runAsync(() -> {
             try {
                 Thread.sleep(2000);
@@ -16,27 +18,32 @@ public class Main {
             System.out.println("Hello World!");
         }).join();*/
 
-
-        CompletableFuture.runAsync(() -> {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
                     try {
                         Thread.sleep(3000);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
-                    System.out.println("Hello ");
+                    return "Hello ";
                 });
-            CompletableFuture.runAsync(() -> {
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("World!");
-            });
 
-            Thread.sleep(6000);
-        }
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "World";
+        });
+
+        BiFunction<String, String, String> function = (value, value2) -> (value + value2); // function to add the stuff
+
+        System.out.println(future1.thenCombine(future2, function).get()); // passing in the stuff
+
+        Thread.sleep(6000);
 
 
+
+    }
 
 }
